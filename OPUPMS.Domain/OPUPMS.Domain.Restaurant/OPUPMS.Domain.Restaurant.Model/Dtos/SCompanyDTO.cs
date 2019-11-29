9d46d1c1c0a4052c764bd79aafc61d1d4599a13f
alexzm19 <36837735+alexzm19@users.noreply.github.com>
2019-11-29 10:48:35 +0800

    1

--- OPUPMS.Domain/OPUPMS.Domain.Restaurant/OPUPMS.Domain.Restaurant.Model/Dtos/SCompanyDTO.cs
+++ OPUPMS.Domain/OPUPMS.Domain.Restaurant/OPUPMS.Domain.Restaurant.Model/Dtos/SCompanyDTO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OPUPMS.Domain.Restaurant.Model.Models;
+using OPUPMS.Domain.Restaurant.Model;
 
 namespace OPUPMS.Domain.Restaurant.Model.Dtos
 {
--- OPUPMS.Domain/OPUPMS.Domain.Restaurant/OPUPMS.Domain.Restaurant.Model/Models/SCompany.cs
+++ OPUPMS.Domain/OPUPMS.Domain.Restaurant/OPUPMS.Domain.Restaurant.Model/Models/SCompany.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OPUPMS.Domain.Restaurant.Model.Models
+namespace OPUPMS.Domain.Restaurant.Model
 {
     public class SCompany
     {
